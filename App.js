2a0a631a26553a20230e41a50c691fcb91e096d2
salma518 <salma.fathi16000@gmail.com>
2025-11-13 18:26:47 +0100

    Props_Usestate_Tp3

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { View, StatusBar, StyleSheet } from 'react-native';
+// import React from 'react';
+// import { View, StatusBar, StyleSheet } from 'react-native';
 // import nommé depuis CardList.js
-import { CardList } from './components/CardList';
+// import { CardList } from './components/CardList';
+
+
+import React, { useState } from 'react';
+import {StyleSheet, View, Button, Text, TextInput } from 'react-native';
 
 export default function App() {
+  // const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+  const [nom , setNom] = useState("");
+
   return (
-    < View style={styles.screen}>
-      <StatusBar />
-      <CardList />
-    </ View >
+    <View style={{ padding: 20 }}>
+      <Text style={Styles.text}>Noms: {list}</Text>
+      <TextInput keyboardType="text" placeholder="saisie le nom ...." 
+      onChangeText={(e) => setNom(e)}></TextInput>
+      <Button title="Ajouter" onPress={() => setList([...list,nom])} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: { flex: 1, backgroundColor: '#f6f7fb' }
+ const  Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  text: {
+    fontSize: 20,
+    color: 'blue',
+    padding:20
+  }
 });
+
+
+
---
+++ components/Props_UseState.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export const  Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  text: {
+    fontSize: 20,
+    color: 'blue'
+  }
+});
